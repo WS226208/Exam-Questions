88bfd3318ee423ce404a40d813702a96e5c4492f
WS226208 <55534100+WS226208@users.noreply.github.com>
2019-11-21 11:05:08 +0000

    Add project files.

---
+++ Exam Questions/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Exam_Questions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMaths = new System.Windows.Forms.Button();
+            this.btnEnglish = new System.Windows.Forms.Button();
+            this.btnProgramming = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblQuestionOne = new System.Windows.Forms.Label();
+            this.txtAnswerOne = new System.Windows.Forms.TextBox();
+            this.lblQuestionTwo = new System.Windows.Forms.Label();
+            this.txtAnswerTwo = new System.Windows.Forms.TextBox();
+            this.txtQuestionThree = new System.Windows.Forms.TextBox();
+            this.lblQuestionThree = new System.Windows.Forms.Label();
+            this.btnAnswers = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotalQuestions = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exam Questions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choose an Exam";
+            // 
+            // btnMaths
+            // 
+            this.btnMaths.Location = new System.Drawing.Point(20, 126);
+            this.btnMaths.Name = "btnMaths";
+            this.btnMaths.Size = new System.Drawing.Size(75, 23);
+            this.btnMaths.TabIndex = 2;
+            this.btnMaths.Text = "Start";
+            this.btnMaths.UseVisualStyleBackColor = true;
+            // 
+            // btnEnglish
+            // 
+            this.btnEnglish.Location = new System.Drawing.Point(379, 126);
+            this.btnEnglish.Name = "btnEnglish";
+            this.btnEnglish.Size = new System.Drawing.Size(75, 23);
+            this.btnEnglish.TabIndex = 3;
+            this.btnEnglish.Text = "Start";
+            this.btnEnglish.UseVisualStyleBackColor = true;
+            // 
+            // btnProgramming
+            // 
+            this.btnProgramming.Location = new System.Drawing.Point(772, 126);
+            this.btnProgramming.Name = "btnProgramming";
+            this.btnProgramming.Size = new System.Drawing.Size(75, 23);
+            this.btnProgramming.TabIndex = 4;
+            this.btnProgramming.Text = "Start";
+            this.btnProgramming.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Maths";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(376, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "English";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(769, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Programming";
+            // 
+            // lblQuestionOne
+            // 
+            this.lblQuestionOne.AutoSize = true;
+            this.lblQuestionOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionOne.Location = new System.Drawing.Point(18, 217);
+            this.lblQuestionOne.Name = "lblQuestionOne";
+            this.lblQuestionOne.Size = new System.Drawing.Size(86, 20);
+            this.lblQuestionOne.TabIndex = 8;
+            this.lblQuestionOne.Text = "Question 1";
+            this.lblQuestionOne.Visible = false;
+            // 
+            // txtAnswerOne
+            // 
+            this.txtAnswerOne.Location = new System.Drawing.Point(22, 240);
+            this.txtAnswerOne.Name = "txtAnswerOne";
+            this.txtAnswerOne.Size = new System.Drawing.Size(279, 20);
+            this.txtAnswerOne.TabIndex = 9;
+            this.txtAnswerOne.Visible = false;
+            // 
+            // lblQuestionTwo
+            // 
+            this.lblQuestionTwo.AutoSize = true;
+            this.lblQuestionTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionTwo.Location = new System.Drawing.Point(18, 298);
+            this.lblQuestionTwo.Name = "lblQuestionTwo";
+            this.lblQuestionTwo.Size = new System.Drawing.Size(86, 20);
+            this.lblQuestionTwo.TabIndex = 10;
+            this.lblQuestionTwo.Text = "Question 2";
+            this.lblQuestionTwo.Visible = false;
+            // 
+            // txtAnswerTwo
+            // 
+            this.txtAnswerTwo.Location = new System.Drawing.Point(22, 321);
+            this.txtAnswerTwo.Name = "txtAnswerTwo";
+            this.txtAnswerTwo.Size = new System.Drawing.Size(279, 20);
+            this.txtAnswerTwo.TabIndex = 11;
+            this.txtAnswerTwo.Visible = false;
+            // 
+            // txtQuestionThree
+            // 
+            this.txtQuestionThree.Location = new System.Drawing.Point(22, 398);
+            this.txtQuestionThree.Name = "txtQuestionThree";
+            this.txtQuestionThree.Size = new System.Drawing.Size(279, 20);
+            this.txtQuestionThree.TabIndex = 13;
+            this.txtQuestionThree.Visible = false;
+            // 
+            // lblQuestionThree
+            // 
+            this.lblQuestionThree.AutoSize = true;
+            this.lblQuestionThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionThree.Location = new System.Drawing.Point(18, 375);
+            this.lblQuestionThree.Name = "lblQuestionThree";
+            this.lblQuestionThree.Size = new System.Drawing.Size(86, 20);
+            this.lblQuestionThree.TabIndex = 12;
+            this.lblQuestionThree.Text = "Question 3";
+            this.lblQuestionThree.Visible = false;
+            // 
+            // btnAnswers
+            // 
+            this.btnAnswers.Location = new System.Drawing.Point(592, 240);
+            this.btnAnswers.Name = "btnAnswers";
+            this.btnAnswers.Size = new System.Drawing.Size(130, 59);
+            this.btnAnswers.TabIndex = 14;
+            this.btnAnswers.Text = "Check Answers";
+            this.btnAnswers.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(589, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Result";
+            this.label6.Visible = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(654, 349);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(23, 25);
+            this.lblScore.TabIndex = 16;
+            this.lblScore.Text = "0";
+            this.lblScore.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(673, 349);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "/";
+            this.label8.Visible = false;
+            // 
+            // lblTotalQuestions
+            // 
+            this.lblTotalQuestions.AutoSize = true;
+            this.lblTotalQuestions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalQuestions.Location = new System.Drawing.Point(691, 348);
+            this.lblTotalQuestions.Name = "lblTotalQuestions";
+            this.lblTotalQuestions.Size = new System.Drawing.Size(23, 25);
+            this.lblTotalQuestions.TabIndex = 18;
+            this.lblTotalQuestions.Text = "3";
+            this.lblTotalQuestions.Visible = false;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(589, 392);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(199, 25);
+            this.lblResults.TabIndex = 19;
+            this.lblResults.Text = "Result Message Here";
+            this.lblResults.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 450);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.lblTotalQuestions);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAnswers);
+            this.Controls.Add(this.txtQuestionThree);
+            this.Controls.Add(this.lblQuestionThree);
+            this.Controls.Add(this.txtAnswerTwo);
+            this.Controls.Add(this.lblQuestionTwo);
+            this.Controls.Add(this.txtAnswerOne);
+            this.Controls.Add(this.lblQuestionOne);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnProgramming);
+            this.Controls.Add(this.btnEnglish);
+            this.Controls.Add(this.btnMaths);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMaths;
+        private System.Windows.Forms.Button btnEnglish;
+        private System.Windows.Forms.Button btnProgramming;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblQuestionOne;
+        private System.Windows.Forms.TextBox txtAnswerOne;
+        private System.Windows.Forms.Label lblQuestionTwo;
+        private System.Windows.Forms.TextBox txtAnswerTwo;
+        private System.Windows.Forms.TextBox txtQuestionThree;
+        private System.Windows.Forms.Label lblQuestionThree;
+        private System.Windows.Forms.Button btnAnswers;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotalQuestions;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
